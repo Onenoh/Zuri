e58768f3fec4f3bd52b2e13e2695a2065670c0db
Onenoh <81997967+Onenoh@users.noreply.github.com>
2022-05-25 22:22:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def is_anagram(s1, s2):
+    if(sorted(s1) == sorted(s2)):
+        return True
+    return False
+
+print(is_anagram("keen","knee"))
+    
+
